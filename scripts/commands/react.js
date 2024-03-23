6914924dcdbd331cc7877e7d0143b5103e8919af
shahariarmahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-03-23 22:48:36 +0600

    Update react.js

--- scripts/commands/react.js
+++ scripts/commands/react.js
@@ -1,24 +1,23 @@
 module.exports.config = {
-	name: "react",
+	name: "message",
 	version: "1.0.0",
 	permission: 2,
 	credits: "ryuko",
-	description: "react posts by id",
-  prefix: true,
+	description: "React to messages by id with specified emoji",
+	prefix: true,
 	category: "admin",
-	usages: "[postID] <reaction type>: (unlike/like/love/heart/haha/wow/sad/angry)",
+	usages: "[messageID] <reaction type>: (unlike/like/love/heart/haha/wow/sad/angry)",
 	cooldowns: 1
 };
 
-
 module.exports.run = async ({ api, event, args }) => {
-  const allType = "unlike/like/love/heart/haha/wow/sad/angry".split("/");
-  const postID = args[0];
-  const type = args[1];
-  if (!postID || !type) return global.utils.throwError(this.config.name, event.threadID, event.messageID);
-  if (!allType.includes(type)) return api.sendMessage(`the reaction type is not valid, please choose one of the following styles : ${allType.join("/")}`, event.threadID, event.messageID);
-  api.setPostReaction(Number(postID), type, (err, data) => {
-    if (err) return api.sendMessage("Something went wrong, please check your postID and try again later", event.threadID, event.messageID);
-    api.sendMessage(`dropped emotion ${type} for posts with id ${postID}`, event.threadID, event.messageID);
-  });
+	const allType = "unlike/like/love/heart/haha/wow/sad/angry".split("/");
+	const messageID = args[0];
+	const type = args[1];
+	if (!messageID || !type) return global.utils.throwError(this.config.name, event.threadID, event.messageID);
+	if (!allType.includes(type)) return api.sendMessage(`অনুগ্রহ করে নিম্নলিখিত স্টাইলগুলির মধ্যে থেকে একটি বেছে নিন: ${allType.join("/")}`, event.threadID, event.messageID);
+	api.setMessageReaction(type, messageID, (err, data) => {
+		if (err) return api.sendMessage("কিছু ভুল হয়েছে, অনুগ্রহ করে আপনার মেসেজ আইডি চেক করুন এবং পরে আবার চেষ্টা করুন", event.threadID, event.messageID);
+		api.sendMessage(`মেসেজের জন্য ${type} রিয়্যাক্ট দেওয়া হয়েছে আইডি ${messageID}`, event.threadID, event.messageID);
+	});
 };
