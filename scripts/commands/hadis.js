87bb924e93d4bfa032d2f7415b16fad594b9309f
shahariarmahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-03-24 10:13:52 +0600

    Update hadis.js

--- scripts/commands/hadis.js
+++ scripts/commands/hadis.js
@@ -1,10 +1,11 @@
 module.exports.config = {
   name: "hadis", // এখানে কমান্ডের নাম দিয়ে দিন
   version: "1.0.0",
-  hasPermssion: 0,
+  permssion: 0,
   credits: "nayan",
+  prefix: true,
   description: "হাদীস প্রদান করে",
-  commandCategory: "general",
+  category: "general",
   usages: "hadis",
   cooldowns: 5,
   dependencies: {
