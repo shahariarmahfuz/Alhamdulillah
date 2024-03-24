341641a9f42ba813df26e79cb61216b305484741
shahariarmahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-03-24 10:04:40 +0600

    Update hadis.js

--- scripts/commands/hadis.js
+++ scripts/commands/hadis.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
 const axios = require('axios');
+const request = require('request'); // এখানে 'request' মডিউল যোগ করা হলো
 
 module.exports.config = {
   name: "hadis",
@@ -13,23 +14,42 @@ module.exports.config = {
   cooldowns: 5,
   dependencies: {
     "fs-extra": "",
-    "axios": ""
+    "axios": "",
+    "request": "" // 'request' মডিউল যোগ করা হলো
   }
 };
 
 function getRandomImage() {
-  // Lorem Picsum ব্যবহার করে র‍্যান্ডম ছবির URL জেনারেট করে
-  const width = 200; // ছবির প্রস্থ
-  const height = 300; // ছবির উচ্চতা
-  return `https://picsum.photos/${width}/${height}`; // র‍্যান্ডম ছবির URL ফেরত পাঠায়
-}
+  var link = [
+    "https://i.postimg.cc/7LdGnyjQ/images-31.jpg",
+  "https://i.postimg.cc/65c81ZDZ/images-30.jpg",
+"https://i.postimg.cc/Y0wvTzr6/images-29.jpg",
+"https://i.postimg.cc/1Rpnw2BJ/images-28.jpg",
+"https://i.postimg.cc/mgrPxDs5/images-27.jpg",
+"https://i.postimg.cc/yxXDK3xw/images-26.jpg",
+"https://i.postimg.cc/kXqVcsh9/muslim-boy-having-worship-praying-fasting-eid-islamic-culture-mosque-73899-1334.webp",
+"https://i.postimg.cc/hGzhj5h8/muslims-reading-from-quran-53876-20958.webp",
+"https://i.postimg.cc/x1Fc92jT/blue-mosque-istanbul-1157-8841.webp",
+"https://i.postimg.cc/j5y56nHL/muhammad-ali-pasha-cairo-219717-5352.webp",
+"https://i.postimg.cc/dVWyHfhr/images-1-21.jpg",
+"https://i.postimg.cc/q7MGgn3X/images-1-22.jpg",
+"https://i.postimg.cc/sX5CXtSh/images-1-16.jpg",
+"https://i.postimg.cc/66Rp2Pwz/images-1-17.jpg",
+"https://i.postimg.cc/Qtzh9pY2/images-1-18.jpg",
+"https://i.postimg.cc/MGrhdz0R/images-1-19.jpg",
+"https://i.postimg.cc/LsMSj9Ts/images-1-20.jpg",
+"https://i.postimg.cc/KzNXyttX/images-1-13.jpg",
+  ];
+  // এখানে 'know' ভ্যারিয়েবলটি ডিফাইন করা হলো
+  var know = 'আপনার হাদীস বা বার্তা';
+  var callback = () => api.sendMessage({body: `「 ${know} 」`, attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));
+  return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close", callback);
+};
 
 module.exports.run = async ({ api, event, args }) => {
-  // এখানে আপনার হাদীস জেনারেটর ফাংশন থাকবে
   const hadisList = [
     'হাদীস ১: বাণী...',
     'হাদীস ২: বাণী...',
-    // আরও হাদীস যোগ করুন
   ];
 
   const randomIndex = Math.floor(Math.random() * hadisList.length);
